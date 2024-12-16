5e1cccdc6a5cd6735877f9692c68ac24d7e6b24a
BelisAliosmanova <benidebelis@gmail.com>
2024-12-16 14:55:17 +0200

    Research descriptions

--- src/main/java/com/lifeAIFrontend/LifeAIFrontend/controller/AnswerController.java
+++ src/main/java/com/lifeAIFrontend/LifeAIFrontend/controller/AnswerController.java
@@ -17,15 +17,34 @@ public class AnswerController {
 
     @PostMapping("/{id}")
     public String getAnswerById(@PathVariable Long id, Model model) {
-        Answer answer = answerClient.getAnswerById(id); // Get the answer from the client
-        model.addAttribute("answer", answer); // Add the answer to the model to be used in the view
-        return "recommendedStudies/questions"; // Return the same view to show the answer
+        Answer answer = answerClient.getAnswerById(id);
+        model.addAttribute("answer", answer);
+        return "recommendedStudies/questions";
     }
 
     @GetMapping("/recommendedStudies")
     public String showQuestions(Model model) {
         model.addAttribute("answer", new Answer());
         model.addAttribute("answers", answerClient.getAllAnswers());
+        model.addAttribute("coreNeedleBiopsy", answerClient.getAnswerById(17L).getText());
+        model.addAttribute("clinicalExamination", answerClient.getAnswerById(18L).getText());
+        model.addAttribute("imagingStudies", answerClient.getAnswerById(19L).getText());
+        model.addAttribute("echomammography", answerClient.getAnswerById(20L).getText());
+        model.addAttribute("ultrasound", answerClient.getAnswerById(21L).getText());
+        model.addAttribute("mammography", answerClient.getAnswerById(22L).getText());
+        model.addAttribute("nuclearMagneticResonance", answerClient.getAnswerById(23L).getText());
+        model.addAttribute("histologicalExaminationOfBiopsyMaterial", answerClient.getAnswerById(24L).getText());
+        model.addAttribute("PETScanner", answerClient.getAnswerById(25L).getText());
+        model.addAttribute("contrastScanner", answerClient.getAnswerById(26L).getText());
+        model.addAttribute("abdominalUltrasound", answerClient.getAnswerById(27L).getText());
+        model.addAttribute("lungX-ray", answerClient.getAnswerById(28L).getText());
+        model.addAttribute("boneScintigraphy", answerClient.getAnswerById(29L).getText());
+        model.addAttribute("tumorMarker", answerClient.getAnswerById(30L).getText());
+        model.addAttribute("BRCA1/2", answerClient.getAnswerById(31L).getText());
+        model.addAttribute("chemotherapy", answerClient.getAnswerById(32L).getText());
+        model.addAttribute("targetedTherapy", answerClient.getAnswerById(32L).getText());
+        model.addAttribute("chemotherapy", answerClient.getAnswerById(32L).getText());
+
         return "recommendedStudies/questions";
     }
 }
