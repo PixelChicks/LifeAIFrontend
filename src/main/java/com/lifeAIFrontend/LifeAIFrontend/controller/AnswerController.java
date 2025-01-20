b4dc87a294a9005adb950f2b08776287de271e5a
BelisAliosmanova <benidebelis@gmail.com>
2025-01-20 11:11:50 +0200

    optimize answer fetching by executing API calls in parallel using CompletableFuture

--- src/main/java/com/lifeAIFrontend/LifeAIFrontend/controller/AnswerController.java
+++ src/main/java/com/lifeAIFrontend/LifeAIFrontend/controller/AnswerController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.concurrent.CompletableFuture;
+
 @Controller
 @AllArgsConstructor
 public class AnswerController {
@@ -26,26 +28,47 @@ public class AnswerController {
     public String showQuestions(Model model) {
         model.addAttribute("answer", new Answer());
         model.addAttribute("answers", answerClient.getAllAnswers());
-        model.addAttribute("coreNeedleBiopsy", answerClient.getAnswerById(17L).getText());
-        model.addAttribute("clinicalExamination", answerClient.getAnswerById(18L).getText());
-        model.addAttribute("imagingStudies", answerClient.getAnswerById(19L).getText());
-        model.addAttribute("echomammography", answerClient.getAnswerById(20L).getText());
-        model.addAttribute("ultrasound", answerClient.getAnswerById(21L).getText());
-        model.addAttribute("mammography", answerClient.getAnswerById(22L).getText());
-        model.addAttribute("nuclearMagneticResonance", answerClient.getAnswerById(23L).getText());
-        model.addAttribute("histologicalExaminationOfBiopsyMaterial", answerClient.getAnswerById(24L).getText());
-        model.addAttribute("PETScanner", answerClient.getAnswerById(25L).getText());
-        model.addAttribute("contrastScanner", answerClient.getAnswerById(26L).getText());
-        model.addAttribute("abdominalUltrasound", answerClient.getAnswerById(27L).getText());
-        model.addAttribute("lungXray", answerClient.getAnswerById(28L).getText());
-        model.addAttribute("boneScintigraphy", answerClient.getAnswerById(29L).getText());
-        model.addAttribute("tumorMarker", answerClient.getAnswerById(30L).getText());
-        model.addAttribute("BRCA12", answerClient.getAnswerById(31L).getText());
-        model.addAttribute("chemotherapy", answerClient.getAnswerById(32L).getText());
-        model.addAttribute("targetedTherapy", answerClient.getAnswerById(33L).getText());
-        model.addAttribute("hormonalTherapy", answerClient.getAnswerById(34L).getText());
-        model.addAttribute("immuneTherapy", answerClient.getAnswerById(35L).getText());
+        // Fetch answers in parallel using CompletableFuture
+        CompletableFuture<String> coreNeedleBiopsyFuture = CompletableFuture.supplyAsync(() -> answerClient.getAnswerById(17L).getText());
+        CompletableFuture<String> clinicalExaminationFuture = CompletableFuture.supplyAsync(() -> answerClient.getAnswerById(18L).getText());
+        CompletableFuture<String> imagingStudiesFuture = CompletableFuture.supplyAsync(() -> answerClient.getAnswerById(19L).getText());
+        CompletableFuture<String> echomammographyFuture = CompletableFuture.supplyAsync(() -> answerClient.getAnswerById(20L).getText());
+        CompletableFuture<String> ultrasoundFuture = CompletableFuture.supplyAsync(() -> answerClient.getAnswerById(21L).getText());
+        CompletableFuture<String> mammographyFuture = CompletableFuture.supplyAsync(() -> answerClient.getAnswerById(22L).getText());
+        CompletableFuture<String> nuclearMagneticResonanceFuture = CompletableFuture.supplyAsync(() -> answerClient.getAnswerById(23L).getText());
+        CompletableFuture<String> histologicalExaminationFuture = CompletableFuture.supplyAsync(() -> answerClient.getAnswerById(24L).getText());
+        CompletableFuture<String> petScannerFuture = CompletableFuture.supplyAsync(() -> answerClient.getAnswerById(25L).getText());
+        CompletableFuture<String> contrastScannerFuture = CompletableFuture.supplyAsync(() -> answerClient.getAnswerById(26L).getText());
+        CompletableFuture<String> abdominalUltrasoundFuture = CompletableFuture.supplyAsync(() -> answerClient.getAnswerById(27L).getText());
+        CompletableFuture<String> lungXrayFuture = CompletableFuture.supplyAsync(() -> answerClient.getAnswerById(28L).getText());
+        CompletableFuture<String> boneScintigraphyFuture = CompletableFuture.supplyAsync(() -> answerClient.getAnswerById(29L).getText());
+        CompletableFuture<String> tumorMarkerFuture = CompletableFuture.supplyAsync(() -> answerClient.getAnswerById(30L).getText());
+        CompletableFuture<String> brca12Future = CompletableFuture.supplyAsync(() -> answerClient.getAnswerById(31L).getText());
+        CompletableFuture<String> chemotherapyFuture = CompletableFuture.supplyAsync(() -> answerClient.getAnswerById(32L).getText());
+        CompletableFuture<String> targetedTherapyFuture = CompletableFuture.supplyAsync(() -> answerClient.getAnswerById(33L).getText());
+        CompletableFuture<String> hormonalTherapyFuture = CompletableFuture.supplyAsync(() -> answerClient.getAnswerById(34L).getText());
+        CompletableFuture<String> immuneTherapyFuture = CompletableFuture.supplyAsync(() -> answerClient.getAnswerById(35L).getText());
 
+        // Wait for all futures to complete and get their results
+        model.addAttribute("coreNeedleBiopsy", coreNeedleBiopsyFuture.join());
+        model.addAttribute("clinicalExamination", clinicalExaminationFuture.join());
+        model.addAttribute("imagingStudies", imagingStudiesFuture.join());
+        model.addAttribute("echomammography", echomammographyFuture.join());
+        model.addAttribute("ultrasound", ultrasoundFuture.join());
+        model.addAttribute("mammography", mammographyFuture.join());
+        model.addAttribute("nuclearMagneticResonance", nuclearMagneticResonanceFuture.join());
+        model.addAttribute("histologicalExaminationOfBiopsyMaterial", histologicalExaminationFuture.join());
+        model.addAttribute("PETScanner", petScannerFuture.join());
+        model.addAttribute("contrastScanner", contrastScannerFuture.join());
+        model.addAttribute("abdominalUltrasound", abdominalUltrasoundFuture.join());
+        model.addAttribute("lungXray", lungXrayFuture.join());
+        model.addAttribute("boneScintigraphy", boneScintigraphyFuture.join());
+        model.addAttribute("tumorMarker", tumorMarkerFuture.join());
+        model.addAttribute("BRCA12", brca12Future.join());
+        model.addAttribute("chemotherapy", chemotherapyFuture.join());
+        model.addAttribute("targetedTherapy", targetedTherapyFuture.join());
+        model.addAttribute("hormonalTherapy", hormonalTherapyFuture.join());
+        model.addAttribute("immuneTherapy", immuneTherapyFuture.join());
 
         return "recommendedStudies/questions";
     }
